19ae56b8dbc556efaead0f353753768c2533236b
Will Williams <wwilliams@indeed.com>
2024-02-26 19:54:08 -0800

    Implements searchByEmail.js

--- src/searchByEmail.js
+++ src/searchByEmail.js
@@ -3,6 +3,13 @@
  * @param {string} email The email address to search for
  * @param {*} customers The array to search
  */
-function searchByEmail(email, customers) {}
+function searchByEmail(email, customers) {
+  for (let i = 0; i < customers.length; i++) {
+    if (customers[i].email === email) {
+      return i; // Return the index if the email is found
+    }
+  }
+  return -1; // Return -1 if the email is not found
+}
 
 module.exports = searchByEmail;
