e271fd9b895ccdc576e24ab388d9dac8aafb7d9e
Will Williams <wwilliams@indeed.com>
2024-02-26 19:28:31 -0800

    Implements sortByName.js

--- src/sortByName.js
+++ src/sortByName.js
@@ -4,6 +4,21 @@ const sort = require("./sort");
  * Sort the array of customers by first and last name
  * @param {array} customers
  */
-function sortByName(customers) {}
+function sortByName(customers) {
+  function compareNames(a, b) {
+    // Compare last names first
+    if (a.lastName < b.lastName) return -1;
+    if (a.lastName > b.lastName) return 1;
+
+    // If last names are the same, compare first names
+    if (a.firstName < b.firstName) return -1;
+    if (a.firstName > b.firstName) return 1;
+
+    return 0;
+  }
+
+  // Sort the customers array using the sort function defined in Task 1
+  return sort(compareNames, customers);
+}
 
 module.exports = sortByName;
